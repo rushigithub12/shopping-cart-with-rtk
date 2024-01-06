a4ecfd390949acaddb31b19440ba988bcdc9a729
rushigithub12 <82263330+rushigithub12@users.noreply.github.com>
2024-01-06 19:36:37 +0530

    json server deployed change api

--- react-rtk-shop/src/feature/cart/cartAPI.js
+++ react-rtk-shop/src/feature/cart/cartAPI.js
@@ -1,17 +1,17 @@
 import axios from "axios"
 
 export const fetchItem = () => {
-    return axios.get(`http://localhost:8081/cart`)
+    return axios.get(`https://shopping-cart-service.onrender.com/cart`)
 }
 
 export const addItem = (item) => {
-    return axios.post(`http://localhost:8081/cart`, item)
+    return axios.post(`https://shopping-cart-service.onrender.com/cart`, item)
 }
 
 export const updateItem = (id, item) => {
-    return axios.put(`http://localhost:8081/cart/${id}`, item)
+    return axios.put(`https://shopping-cart-service.onrender.com/cart/${id}`, item)
 }
 
 export const deleteItem = (id) => {
-    return axios.delete(`http://localhost:8081/cart/${id}`)
+    return axios.delete(`https://shopping-cart-service.onrender.com/cart/${id}`)
 }
